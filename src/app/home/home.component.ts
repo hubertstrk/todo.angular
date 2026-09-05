b74c048b875b51e95ecdd1f53dadd7a7faa46b07
Hubert Stärk <hubert.staerk@gmail.com>
2026-09-05 21:55:18 +0200

    fix spartan ui setup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
-
-import { hlmH1 } from '@spartan-ng/helm/typography';
-
-import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     standalone: true,
-    imports: [RouterLink, TranslatePipe]
+    imports: [CommonModule, RouterLink, TranslatePipe]
 })
 export class HomeComponent implements OnInit {
 
--- src/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import { preset } from '@spartan-ng/brain/hlm-tailwind-preset';
-
-export default {
-  content: [
-    './src/**/*.{html,ts}',
-  ],
-  presets: [preset],
-};
